76353b5fd36796012e8f5e89269a79c25e1f3af9
Leonardo-dev <leonardo.filipe@yahoo.com>
2023-05-01 19:32:20 -0300

    Seu commit message aqui

---
+++ 3-Avancando_tipos/1-arrays/index.js
@@ -0,0 +1,9 @@
+"use strict";
+//1- Array numerico
+let n1 = [1, 2, 3,];
+n1.push(5);
+console.log(2);
+//number = "teste"
+const nomes = ["Mateus", "João"];
+nomes.push("Mateus");
+console.log(nomes[0]);
---
+++ 3-Avancando_tipos/1-arrays/index.ts
@@ -0,0 +1,13 @@
+//1- Array numerico
+
+let n1:number[] = [1, 2, 3,]
+
+n1.push(5)
+
+console.log(2)
+
+//number = "teste"
+
+const nomes:string[] = ["Mateus", "João"] 
+nomes.push("Mateus")
+console.log(nomes[0])
